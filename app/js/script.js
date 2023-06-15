fdfa28a951308b4cd55f9d83227ad38502a4f313
Jaehee Ashley <ja4840@princeton.edu>
2023-06-15 13:16:54 -0500

    added animated hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,20 @@
-console.log("hello, world");
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+    // classList -> class= "sdlkfj lskfjs lsdkfj"
+    // checks if open is part of classList. if yes, remove; else, add
+    if (header.classList.contains('open')) { // close
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } 
+    else { // open
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+});
+
